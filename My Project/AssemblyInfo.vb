a61b5e48f3347cc16bc2d43851328ade94450b67
SergeyT <0tx0rx@gmail.com>
2025-01-03 21:12:21 +0700

    ~ исправление версификатора для правильного подтягивания настроек предыдущих версий

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' используя "*", как показано ниже:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2501.3.2059.32")>
-<Assembly: AssemblyFileVersion("2501.3.2059.32")>
+<Assembly: AssemblyVersion("2501.3.2110.16")>
+<Assembly: AssemblyFileVersion("2501.3.2110.16")>
